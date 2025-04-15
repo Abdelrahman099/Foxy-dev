4b8bebe363934547f2f638537667f1be215ff8da
MrF <aabdelrahman.osama.099@gmail.com>
2025-04-15 05:35:11 +0200

    deployment Vercel Backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://foxy-dev-abdelrahman099s-projects.vercel.app/api';
 
 export const fetchProfile = async () => {
   try {
